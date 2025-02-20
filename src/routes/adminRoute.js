f31854c052b0bdf73953977ea0c58e0f07376574
Prashant <hadaprashant2002@gmail.com>
2025-02-20 18:21:12 +0530

    added more features for admins

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const {userProtect, isAdmin} = require("../middleware/middleware");
-const {getDashboardStats} = require("../controllers/adminController");
+const {getDashboardStats, getAllReservations, getAllUsers} = require("../controllers/adminController");
 
 const router = express.Router();
 
 router.get("/stats", userProtect, isAdmin, getDashboardStats);
+router.get("/users", userProtect, isAdmin, getAllUsers);
+router.get("/reservation", userProtect, isAdmin, getAllReservations);
 
-module.export = router;
+module.exports = router;
